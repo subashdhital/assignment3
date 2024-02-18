cf51a1fd95156a6de5e64b662ca05b473c141745
Subash Dhital <subashdhital06@gmail.com>
2024-02-17 23:50:42 -0500

    final

--- web700-app/server.js
+++ web700-app/server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB700 – Assignment 03
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Subash Dhital Student ID: 12334233 Date: Feb 17, 2024
+*
+********************************************************************************/ 
+
+
 const express = require("express");
 const path = require("path");
 const data = require("./modules/collegeData.js");
